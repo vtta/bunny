ca0890004828cbb9bf7aa64f827f3732ed2a1db1
vtta <vtta0124@gmail.com>
2019-12-16 09:22:52 +0800

    Initial Commit

---
+++ opengl-bunny/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl-bunny
+//
+//  Created by vtta on 12/16/19.
+//  Copyright © 2019 vtta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
